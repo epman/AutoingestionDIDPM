4a186aa29608c417ba6a68c56aca0c85e893789f
Emilio Mancini <epmancini@gmail.com>
2014-09-06 15:53:48 +0200

    download daily data

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ AutoingestionDIDPM
 ==================
 
 Script to download data for Itunes Connect with Autongestion tool (not included)
+
+1 - Download an unzip Autongestion in the script directory
+2 - Update autoingestion.properties with your data
+3 - Rename config.ini.sample to config.ini and update it with your data
+4 - Start getdata.py to download the daily sales information in the data directory
+
+The script recognize the already downloaded files
--- getdata.py
+++ getdata.py
@@ -1,31 +1,53 @@
 #!/usr/bin/python
-VENDOR_ID = 85202719
+from datetime import datetime, timedelta
+from subprocess import call
+import errno
+import os.path
+import ConfigParser
+import shutil
+
+
 
 def getDayDateString(d, m, y):
-  s = str(y)
-  if m < 10:
-    s = s + '0' + str(m)
-  else:
-    s = s + str(m)
-  if d < 10:
-    s = s + '0' + str(d)
-  else:
-    s = s + str(d)
-  return s;
+    s = str(y)
+    if m < 10:
+        s = s + '0' + str(m)
+    else:
+        s = s + str(m)
+    if d < 10:
+        s = s + '0' + str(d)
+    else:
+        s = s + str(d)
+    return s;
   
 
-from datetime import datetime, date, timedelta
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else: raise
+        
+        
+# ------------------------- Main
+config = ConfigParser.RawConfigParser()
+config.read('config.ini')
+VENDOR_ID = config.get('vendor', 'vendorid')
 
+mkdir_p("data")
 now = datetime.now()
-
-mm = now.month
-dd = now.day
-yyyy = now.year
 
 # Daily
 for i in range(1, 26):
-  d=now-timedelta(days=i)
-  print getDayDateString(d.day, d.month,  d.year )
-#java Autoingestion props.properties 85202719 Sales Daily Summary 20140903
-#java Autoingestion props.properties 85202719 Sales Wekely Opt-In 20140902
+    d=now-timedelta(days=i)
+    fdate =  getDayDateString(d.day, d.month,  d.year )
+    fname = 'S_D_' + VENDOR_ID + '_' + fdate + '.txt.gz'
+    fpath = 'data/'+fname
+    if os.path.isfile(fpath):
+        print fpath+' already downloaded'
+    else:
+        print  'java Autoingestion autoingestion.properties ' + VENDOR_ID+ ' Sales Daily Summary '+fdate
+        call(["java", "Autoingestion", "autoingestion.properties", VENDOR_ID, "Sales", "Daily", "Summary", fdate])
+        shutil.move(fname, fpath)
 

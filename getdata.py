da05fd0c540006f3639bc9f2fa4ed988bbe1c960
epmancini <epmancini@gmail.com>
2014-09-25 23:26:12 -0400

    Verbose flag

--- getdata.py
+++ getdata.py
@@ -5,6 +5,7 @@ import errno
 import os.path
 import ConfigParser
 import shutil
+import argparse
 
 
 def getMonthDateString(m, y):
@@ -38,6 +39,10 @@ def mkdir_p(path):
         
         
 # ------------------------- Main
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--verbose', action='store_true', help='verbose')
+args = parser.parse_args()
+
 config = ConfigParser.RawConfigParser()
 config.read('config.ini')
 VENDOR_ID = config.get('vendor', 'vendorid')
@@ -52,24 +57,29 @@ mkdir_p(DATA_DIR_YEARLY)
 now = datetime.now()
 
 sunday = now - timedelta(days=now.weekday()) + timedelta(days=6)
-# Weekly
-print "Download weekly reports"
-for i in range(1, 27):
-    sunday = sunday + timedelta(weeks=-1)
-    fdate =  getDayDateString(sunday.day, sunday.month,  sunday.year )
-    fname = 'S_W_' + VENDOR_ID + '_' + fdate + '.txt.gz'
-    fpath = DATA_DIR_WEEKLY+'/'+fname
+
+# Yearly
+if  args.verbose:
+    print "Download yearly reports"
+year = now.year
+while (year>2010):
+    year = year - 1
+    fname = 'S_Y_' + VENDOR_ID + '_' + str(year) + '.txt.gz'
+    fpath = DATA_DIR_YEARLY+'/'+fname
     if os.path.isfile(fpath):
-        print fpath+' already downloaded'
+        if  args.verbose:
+            print fpath+' already downloaded'
     else:
-        print  'java Autoingestion autoingestion.properties ' + VENDOR_ID+ ' Sales Weekly Summary '+fdate
-        call(["java", "Autoingestion", "autoingestion.properties", VENDOR_ID, "Sales", "Weekly", "Summary", fdate])
+        if  args.verbose:        
+            print  'java Autoingestion autoingestion.properties ' + VENDOR_ID+ ' Sales Yearly Summary '+str(year)
+        call(["java", "Autoingestion", "autoingestion.properties", VENDOR_ID, "Sales", "Yearly", "Summary", str(year)])
         if os.path.isfile(fname):
             shutil.move(fname, fpath)
 
 
 # Monthly
-print "Download monthly reports"
+if  args.verbose:
+    print "Download monthly reports"
 month = now.month
 year = now.year
 for i in range(1, 13):
@@ -81,39 +91,50 @@ for i in range(1, 13):
     fname = 'S_M_' + VENDOR_ID + '_' + fdate + '.txt.gz'
     fpath = DATA_DIR_MONTHLY+'/'+fname
     if os.path.isfile(fpath):
-        print fpath+' already downloaded'
+        if  args.verbose:
+            print fpath+' already downloaded'
     else:
-        print  'java Autoingestion autoingestion.properties ' + VENDOR_ID+ ' Sales Monthly Summary '+fdate
+        if  args.verbose:
+            print  'java Autoingestion autoingestion.properties ' + VENDOR_ID+ ' Sales Monthly Summary '+fdate
         call(["java", "Autoingestion", "autoingestion.properties", VENDOR_ID, "Sales", "Monthly", "Summary", fdate])
         if os.path.isfile(fname):
             shutil.move(fname, fpath)
 
-# Yearly
-print "Download yearly reports"
-year = now.year
-while (year>2010):
-    year = year - 1
-    fname = 'S_Y_' + VENDOR_ID + '_' + str(year) + '.txt.gz'
-    fpath = DATA_DIR_YEARLY+'/'+fname
+
+# Weekly
+if  args.verbose:
+    print "Download weekly reports"
+for i in range(1, 27):
+    sunday = sunday + timedelta(weeks=-1)
+    fdate =  getDayDateString(sunday.day, sunday.month,  sunday.year )
+    fname = 'S_W_' + VENDOR_ID + '_' + fdate + '.txt.gz'
+    fpath = DATA_DIR_WEEKLY+'/'+fname
     if os.path.isfile(fpath):
-        print fpath+' already downloaded'
+        if  args.verbose:
+            print fpath+' already downloaded'
     else:
-        print  'java Autoingestion autoingestion.properties ' + VENDOR_ID+ ' Sales Yearly Summary '+str(year)
-        call(["java", "Autoingestion", "autoingestion.properties", VENDOR_ID, "Sales", "Yearly", "Summary", str(year)])
+        if  args.verbose:
+            print  'java Autoingestion autoingestion.properties ' + VENDOR_ID+ ' Sales Weekly Summary '+fdate
+        call(["java", "Autoingestion", "autoingestion.properties", VENDOR_ID, "Sales", "Weekly", "Summary", fdate])
         if os.path.isfile(fname):
             shutil.move(fname, fpath)
 
+
+
 # Daily
-print "Download daily reports"
+if  args.verbose:
+    print "Download daily reports"
 for i in range(1, 30):
     d=now-timedelta(days=i)
     fdate =  getDayDateString(d.day, d.month,  d.year )
     fname = 'S_D_' + VENDOR_ID + '_' + fdate + '.txt.gz'
     fpath = DATA_DIR_DAILY+'/'+fname
     if os.path.isfile(fpath):
-        print fpath+' already downloaded'
+        if  args.verbose:
+            print fpath+' already downloaded'
     else:
-        print  'java Autoingestion autoingestion.properties ' + VENDOR_ID+ ' Sales Daily Summary '+fdate
+        if  args.verbose:
+            print  'java Autoingestion autoingestion.properties ' + VENDOR_ID+ ' Sales Daily Summary '+fdate
         call(["java", "Autoingestion", "autoingestion.properties", VENDOR_ID, "Sales", "Daily", "Summary", fdate])
     if os.path.isfile(fname):
             shutil.move(fname, fpath)
